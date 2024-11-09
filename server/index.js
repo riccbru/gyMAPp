64d46ca902e51c8d0650130e214461565f8abacb
ciclarimpo <74079434+ciclarimpo@users.noreply.github.com>
2024-11-09 02:21:58 +0100

    FIX: POST BIA

--- server/index.js
+++ server/index.js
@@ -4,7 +4,6 @@ const cors = require("cors");
 const morgan = require("morgan");
 const express = require("express");
 const passport = require("passport");
-const bodyParser = require("body-parser");
 const session = require("express-session");
 const LocalStrategy = require("passport-local");
 const { body, check, param, validationResult } = require("express-validator");
@@ -30,24 +29,27 @@ const corsOptions = {
 };
 
 const sessionOptions = {
-    secret: "CHANGE_IN_ENV_VAR",
-    resave: false,
-    saveUninitialized: false,
-    cookie: { httpOnly: true, secure: app.get('env') === 'production' ? true : false },
+  secret: "CHANGE_IN_ENV_VAR",
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    httpOnly: true,
+    secure: app.get("env") === "production" ? true : false,
+  },
 };
 
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
-// app.use(express.json());
-app.use(bodyParser.json({limit: '50mb'}));
-app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+app.use(express.json());
+// app.use(bodyParser.json({ limit: "50mb" }));
+// app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 
 app.use(cors(corsOptions));
 app.use(session(sessionOptions));
-app.use(passport.authenticate('session'));
+app.use(passport.authenticate("session"));
 
 const errorFormatter = ({ location, msg }) => {
-    return `${msg}, check ${location}`;
+  return `${msg}, check ${location}`;
 };
 
 const isLogged = (req, res, next) => {
@@ -67,8 +69,8 @@ const isAdmin = (req, res, next) => {
 };
 
 app.use((err, req, res, next) => {
-    console.error(err.stack);  // Log the error stack trace
-    res.status(500).send('Internal Server Error');  // Return a 500 error to client
+  console.error(err.stack); // Log the error stack trace
+  res.status(500).send("Internal Server Error"); // Return a 500 error to client
 });
 
 app.listen(PORT, () => {
@@ -84,10 +86,10 @@ app.listen(PORT, () => {
 passport.use(
   new LocalStrategy(async function verify(username, password, callback) {
     try {
-        const user = await daoUsers.login({ username, password });
-        return callback(null, user);
-    } catch(err) {
-        return callback(null, false, "Incorrect username and/or password");
+      const user = await daoUsers.login({ username, password });
+      return callback(null, user);
+    } catch (err) {
+      return callback(null, false, "Incorrect username and/or password");
     }
   })
 );
@@ -104,170 +106,152 @@ passport.deserializeUser(function (user, callback) {
 /*** AuthN APIs ***/
 /******************/
 
-app.get("/api/session",
-    (req, res) => {
-        if (req.isAuthenticated()) {
-            res.status(200).json(req.user);
-        } else {
-            res.status(401).json({ error: "No active session" });
-        }
-    }
-);
+app.get("/api/session", (req, res) => {
+  if (req.isAuthenticated()) {
+    res.status(200).json(req.user);
+  } else {
+    res.status(401).json({ error: "No active session" });
+  }
+});
 
-app.post("/api/login",
-    function (req, res, next) {
-    passport.authenticate("local", (err, user, info) => {
+app.post("/api/login", function (req, res, next) {
+  passport.authenticate("local", (err, user, info) => {
+    if (err) return next(err);
+    if (!user) {
+      return res.status(401).json({ error: info });
+    }
+    req.login(user, (err) => {
       if (err) return next(err);
-      if (!user) {
-        return res.status(401).json({ error: info });
+      return res.json(req.user);
+    });
+  })(req, res, next);
+});
+
+app.post(
+  "/api/signup",
+  [
+    body("name")
+      .notEmpty()
+      .withMessage("Your name can't be an empty string")
+      .isString()
+      .withMessage("Name must be string")
+      .isLength({ min: 1, max: maxUserLength })
+      .withMessage("Name length must be between 1 and 20"),
+    body("email")
+      .notEmpty()
+      .withMessage("Your email can't be an empty string")
+      .isString()
+      .withMessage("Last time I checked emails were strings")
+      .isLength({ min: minEmailChars })
+      .withMessage("Your email can't be that short")
+      .isEmail()
+      .withMessage("Invalid email: user@domain.tld"),
+    body("birthdate")
+      .notEmpty()
+      .withMessage("Your birthdate can't be an empty string")
+      .isString()
+      .withMessage("Birthdate must be a string")
+      .isLength({ min: minDateChars })
+      .withMessage("Invalid birthdate")
+      .matches(/^(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])-\d{4}$/)
+      .withMessage("Birthdate must be in MM-DD-YYYY format")
+      .bail()
+      .isDate({ format: "MM-DD-YYYY", strictMode: true })
+      .withMessage("Invalid birthdate"),
+    body("username")
+      .notEmpty()
+      .withMessage("Your username can't be an empty string")
+      .isString()
+      .withMessage("Username must be a string")
+      .isLength({ max: maxUserLength })
+      .withMessage(`Username cannot exceed ${maxUserLength} characters`),
+    body("password")
+      .notEmpty()
+      .withMessage("Your password can't be an empty string")
+      .isString()
+      .withMessage("Password must be a string")
+      .isLength({ min: minPassLength })
+      .withMessage(`Password must be at least ${minPassLength} characters`),
+  ],
+  async (req, res) => {
+    if (req.isAuthenticated()) {
+      return res.status(403);
+    } else {
+      const errors = validationResult(req).formatWith(errorFormatter);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ error: errors.array()[0] });
       }
-      req.login(user, (err) => {
-        if (err) return next(err);
-        return res.json(req.user);
-      });
-    })(req, res, next);
+      const user = {
+        name: req.body.name,
+        email: req.body.email,
+        username: req.body.username,
+        password: req.body.password,
+        birthdate: req.body.birthdate,
+      };
+      try {
+        const result = await daoUsers.signup(user);
+        if (!result) return res.status(500).json();
+        return res.status(200).json({ success: result });
+      } catch (err) {
+        return res.status(409).json({ error: err });
+      }
     }
+  }
 );
 
-app.post("/api/signup",
-    [
-        body('name')
-            .notEmpty().withMessage("Your name can't be an empty string")
-            .isString().withMessage("Name must be string")
-            .isLength({ min: 1, max: maxUserLength })
-            .withMessage("Name length must be between 1 and 20"),
-        body('email')
-            .notEmpty().withMessage("Your email can't be an empty string")
-            .isString().withMessage("Last time I checked emails were strings")
-            .isLength({ min: minEmailChars }).withMessage("Your email can't be that short")
-            .isEmail().withMessage("Invalid email: user@domain.tld"),
-        body('birthdate')
-            .notEmpty().withMessage("Your birthdate can't be an empty string")
-            .isString().withMessage("Birthdate must be a string")
-            .isLength({ min: minDateChars }).withMessage("Invalid birthdate")
-            .matches(/^(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])-\d{4}$/)
-            .withMessage("Birthdate must be in MM-DD-YYYY format")
-            .bail()
-            .isDate({ format: 'MM-DD-YYYY', strictMode: true })
-            .withMessage("Invalid birthdate"),
-        body('username')
-            .notEmpty().withMessage("Your username can't be an empty string")
-            .isString().withMessage("Username must be a string")
-            .isLength({ max: maxUserLength })
-            .withMessage(`Username cannot exceed ${maxUserLength} characters`),
-        body('password')
-            .notEmpty().withMessage("Your password can't be an empty string")
-            .isString().withMessage("Password must be a string")
-            .isLength({ min: minPassLength })
-            .withMessage(`Password must be at least ${minPassLength} characters`)
-    ],
-    async (req, res) => {
-        if (req.isAuthenticated()) {
-            return res.status(403);
-        } else {
-            const errors = validationResult(req).formatWith(errorFormatter);
-            if (!errors.isEmpty()) {
-                return res.status(400).json({ error: errors.array()[0] });
-            }
-            const user = {
-                name:       req.body.name,
-                email:      req.body.email,
-                username:   req.body.username,
-                password:   req.body.password,
-                birthdate:  req.body.birthdate,
-            };
-            try {
-                const result = await daoUsers.signup(user)
-                if (!result) return res.status(500).json();
-                return res.status(200).json({ success: result});
-            } catch (err) {
-                return res.status(409).json({ error: err });
-            }
-        }
-    }
-);
-
-app.get("/api/logout", isLogged,
-    (req, res) => {
-        req.logout(() => {
-          res.status(200).json({ success: "Logged out" });
-        });
-    }
-);
+app.get("/api/logout", isLogged, (req, res) => {
+  req.logout(() => {
+    res.status(200).json({ success: "Logged out" });
+  });
+});
 
 /*******************/
 /***   BIA API   ***/
 /*******************/
 
-app.get("/api/bias/:uid", isLogged,
+app.get("/api/bia/", isLogged,
+  async (req, res) => {
+    const errors = validationResult(req).formatWith(errorFormatter);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ error: errors.array()[0] });
+    }
+      try {
+        const bias = await daoBias.fetchBias(req.user.uid);
+        res.status(200).json(bias);
+      } catch (err) {
+        res.status(404).json({ error: err });
+      }
+  }
+);
+
+app.post("/api/bia/", isLogged,
   [
-    check("uid").notEmpty().withMessage("Provide a UID"),
-    param("uid").isInt({ min: 1 }).withMessage("UID starts from 1").toInt()
+    body("date")
+      .notEmpty()
+      .withMessage("Your date can't be an empty string")
+      .isString()
+      .withMessage("Date must be a string")
+      .isLength({ min: minDateChars })
+      .withMessage("Invalid date")
+      .matches(/^(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])-\d{4}$/)
+      .withMessage("date must be in MM-DD-YYYY format")
+      .bail()
+      .isDate({ format: "MM-DD-YYYY", strictMode: true })
+      .withMessage("Invalid date"),
   ],
   async (req, res) => {
     const errors = validationResult(req).formatWith(errorFormatter);
     if (!errors.isEmpty()) {
       return res.status(400).json({ error: errors.array()[0] });
     }
-    if (req.params.uid === req.user.uid) {
-      const uid = req.user.uid;
-      try {
-        const bias = await daoBias.fetchBias(uid);
-        res.status(200).json(bias);
-      } catch (err) {
-        res.status(404).json({ error: err });
-      }
-    } else {
-      return res.status(403).json({error: "You can't access other users' BIA"});
-    }
-  }
-);
-
-app.post("/api/bias/",
-  [
-    body('date')
-            .notEmpty().withMessage("Your date can't be an empty string")
-            .isString().withMessage("Date must be a string")
-            .isLength({ min: minDateChars }).withMessage("Invalid date")
-            .matches(/^(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])-\d{4}$/)
-            .withMessage("date must be in MM-DD-YYYY format")
-            .bail()
-            .isDate({ format: 'MM-DD-YYYY', strictMode: true })
-            .withMessage("Invalid date")
-  ],
-  async (req, res) => {
-    const errors = validationResult(req).formatWith(errorFormatter);
-    if (!errors.isEmpty()) {
-        return res.status(400).json({ error: errors.array()[0] });
-    }
-    if (req.isAuthenticated()) {
-      const uid = req.user.uid;
-      try {
-        const bia = {
-          uid:                                req.user.uid,
-          bmi:                                req.body.bmi,
-          date:                               req.body.date,
-          na_k:                               req.body.na_k,
-          weight:                             req.body.weight,
-          height:                             req.body.height,
-          fat_mass:                           req.body.fat_mass,
-          muscle_mass:                        req.body.muscle_mass,
-          phase_angle:                        req.body.phase_angle,
-          fat_free_mass:                      req.body.fat_free_mass,
-          total_body_water:                   req.body.total_body_water,
-          basal_metabolic_rate:               req.body.basal_metabolic_rate,
-          extra_cellular_water:               req.body.extra_cellular_water,
-          intra_cellular_water:               req.body.intra_cellular_water,
-          skeletal_muscle_mass:               req.body.skeletal_muscle_mass,
-          body_composition_measurement:       req.body.body_composition_measurement,
-          total_daily_energy_expenditure:     req.body.total_daily_energy_expenditure,
-          appendicular_skeletal_muscle_mass:  req.body.appendicular_skeletal_muscle_mass,
-        }
-        const call = await daoBias.pushBia(bia);
-        return res.status(200).json({ success: call });
-      } catch (err) {
-        return res.status(403).json({error: "You can't push other users' BIA"});
-      }
+    const uid = req.user.uid;
+    console.log(req.body);
+    try {
+      const bia = {uid: req.user.uid, ...req.body}
+      const call = await daoBias.pushBia(bia);
+      return res.status(200).json({ success: call });
+    } catch (err) {
+      return res.status(500).json({ error: err });
     }
   }
 );
