c5e5366354ccce111f0567e52d7943846924bf76
ciclarimpo <74079434+ciclarimpo@users.noreply.github.com>
2024-11-11 18:51:07 +0100

    UPDATE: Meals DB insertion (lunch & dinner left)

---
+++ server/daoMeals.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const db = require("./db");
+
+const returnMeal = (m) => ({
+    ingredient:     m.ingredient_name,
+    quantity:       m.quantity
+});
+
+exports.fetchMeal = (uid, weekday, meal_type) => {
+    return new Promise((resolve, reject) => {
+        const sql = `
+            SELECT
+                m.id AS meal_id,
+                m.weekday,
+                m.meal_type,
+                i.ingredient_name,
+                iu.quantity
+            FROM
+                meals m
+            JOIN
+                ingredients_usage iu ON m.id = iu.meal_id
+            JOIN
+                ingredients i ON iu.ingredient_id = i.id
+            WHERE
+                m.uid = ?
+                AND m.weekday = ?
+                AND m.meal_type = ?;
+            ORDER BY
+                i.ingredient_id;
+        `;
+        db.get("SELECT * FROM users WHERE uid = ?", [uid], (err, row) => {
+            if (err) { reject(err); }
+            else if (!row) { reject(`User with UID #${uid} does not exist`); }
+        });
+        db.all(sql, [uid, weekday, meal_type], (err, rows) => {
+            if (err) { reject(err); }
+            else if (!rows.length) { reject(`Meal not found`); }
+            else {
+                const meal = rows.map((m) => returnMeal(m));
+                resolve(meal);
+            }
+        });
+    });
+};
