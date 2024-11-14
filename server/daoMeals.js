6af4e8b2892b2a7938780a562e8dba2ef64e1bfb
ciclarimpo <74079434+ciclarimpo@users.noreply.github.com>
2024-11-14 01:55:58 +0100

    UPDATE: meals table complete

--- server/daoMeals.js
+++ server/daoMeals.js
@@ -2,16 +2,8 @@
 
 const db = require("./db");
 
-const returnMeal = (m) => ({
-    ingredient:     m.ingredient_name,
-    quantity:       m.quantity
-});
-
-const returnMealWithOptions = (uid, weekday, meal_type, rows) => {
+const returnMeal = (rows) => {
     const meals = {
-        uid: uid,
-        meal: parseInt(meal_type, 10),
-        weekday: parseInt(weekday, 10),
         options: {}
     };
     rows.forEach(row => {
@@ -29,43 +21,6 @@ const returnMealWithOptions = (uid, weekday, meal_type, rows) => {
 };
 
 exports.fetchMeal = (uid, weekday, meal_type) => {
-    return new Promise((resolve, reject) => {
-        const sql = `
-            SELECT
-                m.id AS meal_id,
-                m.weekday,
-                m.meal_type,
-                i.ingredient_name,
-                iu.quantity
-            FROM
-                meals m
-            JOIN
-                ingredients_usage iu ON m.id = iu.meal_id
-            JOIN
-                ingredients i ON iu.ingredient_id = i.id
-            WHERE
-                m.uid = ?
-                AND m.weekday = ?
-                AND m.meal_type = ?;
-            ORDER BY
-                i.ingredient_id;
-        `;
-        db.get("SELECT * FROM users WHERE uid = ?", [uid], (err, row) => {
-            if (err) { reject(err); }
-            else if (!row) { reject(`User with UID #${uid} does not exist`); }
-        });
-        db.all(sql, [uid, weekday, meal_type], (err, rows) => {
-            if (err) { reject(err); }
-            else if (!rows.length) { reject(`Meal not found`); }
-            else {
-                const meal = rows.map((m) => returnMeal(m));
-                resolve(meal);
-            }
-        });
-    });
-};
-
-exports.fetchMealWithOptions = (uid, weekday, meal_type) => {
     return new Promise((resolve, reject) => {
         const sql = `
         SELECT 
@@ -86,13 +41,13 @@ exports.fetchMealWithOptions = (uid, weekday, meal_type) => {
             AND m.weekday = ?
             AND m.meal_type = ?
         ORDER BY 
-            iu.option_id, i.ingredient_name;
+            iu.option_id;
         `;
         db.all(sql, [uid, weekday, meal_type], (err, rows) => {
             if (err) { reject(err); }
             else if (!rows.length) { reject(`Meal not found`); }
             else {
-                const meal = returnMealWithOptions(uid, weekday, meal_type, rows);
+                const meal = returnMeal(rows);
                 resolve(meal);
             }
         });
