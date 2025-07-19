6978aa87d7650e7643bff6d0db2652f0714a40a5
riccbru <74079434+riccbru@users.noreply.github.com>
2025-07-19 17:44:38 +0200

    FIX: removed data/ from .gitignore

---
+++ client/src/data/pdfFiles.js
@@ -0,0 +1,18 @@
+"use strict";
+
+const files = {
+    bias: [
+      { title: "BIA #2", author: "Mattia Cecchetti", path: "/pdf/bia_2.pdf", description: "December 19, 2024" },
+      { title: "BIA #1", author: "Mattia Cecchetti", path: "/pdf/bia_1.pdf", description: "August 31, 2024" }
+    ],
+    meals: [
+      { title: "MEAL #2", author: "Mattia Cecchetti", path: "/pdf/meal_2.pdf", description: "January 2, 2025" },
+      { title: "MEAL #1", author: "Mattia Cecchetti", path: "/pdf/meal_1.pdf", description: "September 5, 2024" },
+    ],
+    workouts: [
+      { title: "WORKOUT #1", author: "Emanuele Moretti", path: "/pdf/workout_1.pdf", description: "October 23, 2024" },
+    ]
+};
+
+export { files };
+
